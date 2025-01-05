935a7bf816193cc10b7e90422122c171c8c84a55
samarhirau <samrhirau@gmail.com>
2025-01-05 23:40:00 +0530

    start frontend and login page

---
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CaptainLogin() {
+  return (
+    <div>
+      CaptainLogin
+    </div>
+  )
+}
+
+export default CaptainLogin
---
+++ Frontend/src/pages/UserSignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserSignup() {
+  return (
+    <div>
+      signup
+    </div>
+  )
+}
+
+export default UserSignup
